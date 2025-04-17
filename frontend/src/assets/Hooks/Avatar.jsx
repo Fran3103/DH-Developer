51967c180060691f032b4a9e23cdbc3722c8a823
Fran3103 <fn.aguirre@hotmail.com>
2025-04-17 12:09:05 -0300

    añadiendo caracteristicas a producto

--- frontend/src/assets/Hooks/Avatar.jsx
+++ frontend/src/assets/Hooks/Avatar.jsx
@@ -18,7 +18,7 @@ export const Avatar = () => {
       .toUpperCase() || "";
 
   const avatar = name + lastName;
-    // console.log(avatar)
+  
   return (
     <>
       <span className="w-10 h-10 flex items-center justify-center rounded-full bg-celeste text-white text-lg font-semibold">
