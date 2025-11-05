e2b10af1fe582fc4025de71ddd301433c71f3a5f
Fran3103 <fn.aguirre@hotmail.com>
2025-11-05 10:43:32 -0300

    actualizando formateo del codigo

--- frontend/src/assets/features/categorias/Categorias.jsx
+++ frontend/src/assets/features/categorias/Categorias.jsx
@@ -1,54 +1,51 @@
 import { Link } from "react-router-dom";
 import { IoFilter } from "react-icons/io5";
 import { useEffect, useState } from "react";
-import { getCategorias } from "../categorias/CategoriaService"
+import { getCategorias } from "../categorias/CategoriaService";
 
 const Categorias = () => {
   const [datos, setDatos] = useState([]);
-  const [categoriasTodas, setCategoriasTodas] = useState([]); 
+  const [categoriasTodas, setCategoriasTodas] = useState([]);
 
   useEffect(() => {
     fetch("http://localhost:3000/productos/filter?size=1000")
       .then((resp) => resp.json())
       .then((page) => setDatos(page.content));
-
-    
   }, []);
 
-useEffect(() => async () => {
-    try {await getCategorias()
-      .then((res) => setCategoriasTodas(res))
-      }
-      catch (error) {
+  useEffect(
+    () => async () => {
+      try {
+        await getCategorias().then((res) => setCategoriasTodas(res));
+      } catch (error) {
         console.error("Error al obtener categorías:", error);
       }
-  },[]);
+    },
+    []
+  );
 
- 
-
-
-  
   const categoriasMap = new Map();
-datos.forEach(producto => {
-  const catImages = categoriasTodas.filter(cat => cat.id === producto.categorias[0].id);
- 
-  producto.categorias.forEach(catObj => {
-    const cat = catObj.name;    // aquí sí es un string
+  datos.forEach((producto) => {
+    const catImages = categoriasTodas.filter(
+      (cat) => cat.id === producto.categorias[0].id
+    );
 
-    if (!categoriasMap.has(cat)) {
-      categoriasMap.set(cat, {
-        categoria: cat,
-        imagen: catImages[0].image,
-        cantidad: 1,
-      });
-    } else {
-      categoriasMap.get(cat).cantidad++;
-    }
+    producto.categorias.forEach((catObj) => {
+      const cat = catObj.name; // aquí sí es un string
+
+      if (!categoriasMap.has(cat)) {
+        categoriasMap.set(cat, {
+          categoria: cat,
+          imagen: catImages[0].image,
+          cantidad: 1,
+        });
+      } else {
+        categoriasMap.get(cat).cantidad++;
+      }
+    });
   });
-});
 
   const categorias = Array.from(categoriasMap.values());
-
 
   return (
     <div className="m-auto  mt-16 md:mt-20">
@@ -58,14 +55,19 @@ datos.forEach(producto => {
             Categorias
           </h1>
 
-          <Link to={'/productos/filter?'} className="text-xs w-20 flex gap-2">ver más<IoFilter className="text-xl"/></Link>
+          <Link to={"/productos/filter?"} className="text-xs w-20 flex gap-2">
+            ver más
+            <IoFilter className="text-xl" />
+          </Link>
         </div>
         <div className="flex  flex-col justify-between items-center  w-full m-auto gap-3  max-w-[1240px] xs:flex-wrap xs:flex-row xs:gap-3 sm:flex-row  sm:gap-3">
-          {categorias.slice(0,4).map(({categoria, imagen, cantidad}) => {
+          {categorias.slice(0, 4).map(({ categoria, imagen, cantidad }) => {
             return (
               <Link
                 key={categoria}
-                to={`/productos/filter?category=${encodeURIComponent(categoria)}`}
+                to={`/productos/filter?category=${encodeURIComponent(
+                  categoria
+                )}`}
                 className="w-80  xs:w-40 sm:w-48 md:w-56 "
               >
                 <div className="flex flex-col  max-h-72  bg-white rounded-lg border shadow-lg hover:shadow-xl ">
@@ -75,9 +77,7 @@ datos.forEach(producto => {
                     className="w-full h-24 lg:h-32 rounded-t-lg "
                   />
                   <div className="p-1 bg-white h-11 rounded-b-lg">
-                    <h3 className="text-xs font-bold text-azul">
-                      {categoria}
-                    </h3>
+                    <h3 className="text-xs font-bold text-azul">{categoria}</h3>
                     <p className="text-[10px] text-gray-800">
                       {cantidad} {categoria}
                     </p>
